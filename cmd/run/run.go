5a005cacdd474682405593ec4293593d221f7388
dannypsnl <dannypsnl@gmail.com>
2019-06-01 10:19:41 +0800

    fix, stdin should be redirect to os.Stdin

--- cmd/run/run.go
+++ cmd/run/run.go
@@ -37,7 +37,7 @@ func NewParentProcess(tty bool, cmd string) *exec.Cmd {
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET | syscall.CLONE_NEWIPC,
 	}
 	if tty {
-		//command.Stdin = os.Stdin
+		command.Stdin = os.Stdin
 		command.Stdout = os.Stdout
 		command.Stderr = os.Stderr
 	}
